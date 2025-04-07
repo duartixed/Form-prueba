2f194ea9af84275385e8830e2b5609449ee6359a
duartixed <duartixed@hotmail.com>
2025-04-06 21:40:59 -0500

    :sparkles: subiendo configuración de maquina virtual azure-deploy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('*', (req, res) => {
 });
 
 //Expose
-const PORT = process.env.PORT || 8000; 
+const PORT = process.env.PORT || 8080; 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
