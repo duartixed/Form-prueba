6933dc0d92cb46168e41dbc5c0f89c6e9cc19fcf
duartixed <duartixed@hotmail.com>
2025-04-24 22:51:14 -0500

    Actualizando

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('*', (req, res) => {
 });
 
 //Expose
-const PORT = process.env.PORT || 8080; 
+const PORT = process.env.PORT || 8000; 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
